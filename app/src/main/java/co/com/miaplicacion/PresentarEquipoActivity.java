a214b77329d5b92648ae829efc12e8218202fbbc
karinagth1 <karina_pineda97@hotmail.com>
2020-08-25 22:16:12 -0500

    Presentacion equipo

--- app/src/main/java/co/com/miaplicacion/PresentarEquipoActivity.java
+++ app/src/main/java/co/com/miaplicacion/PresentarEquipoActivity.java
@@ -3,12 +3,17 @@ package co.com.miaplicacion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class PresentarEquipoActivity extends AppCompatActivity {
-
+    private TextView Integrante_1;
+    private TextView Integrante_2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presentar_equipo);
+        Integrante_1= findViewById(R.id.editTextTextPersonName4);
+        Integrante_2= findViewById(R.id.editTextTextPersonName5);
     }
 }
